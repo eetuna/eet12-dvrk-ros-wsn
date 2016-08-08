a3963cc9385f13454da89bae1bb36d828479df1c
eetuna <eetuna@gmail.com>
2016-08-08 10:57:18 -0400

    BodyJacobian IK FK Python wrapper tests done

--- dvrk_kinematics/src/psm_logic.cpp
+++ dvrk_kinematics/src/psm_logic.cpp
@@ -36,9 +36,10 @@ http://www.cisst.org/cisst/license.txt.
 
 #include "dvrk_kinematics/psm_logic.h"
 
+using namespace std;
 // set up joint state variables
-static vctDoubleVec psm_joint_current;
-static vctDoubleVec psm_joint_command;
+static vctDoubleVec psm_joint_current, psm_joint_currentTest;
+static vctDoubleVec psm_joint_command, psm_joint_currentIK;
 static vctDoubleVec psm_joint_command_prev;
 static vctFrm4x4 psm_pose_current;
 static vctFrm4x4 psm_pose_command;
@@ -46,201 +47,161 @@ static double mtm_gripper;
 static int control_mode;
 
 
-// command psm pose from teleop
-void psm_cmd_pose_cb(const geometry_msgs::Pose &msg)
-{
-    mtsROSToCISST(msg, psm_pose_command);
-}
-
-// mtm gripper open angle
-void mtm_gripper_cb(const std_msgs::Float32 &msg)
-{
-    mtm_gripper = msg.data;
-}
-
-// psm joint feedback from RVIZ (joint_state_publisher)
-void psm_joint_feedback_cb(const sensor_msgs::JointState &msg)
-{
-    if (control_mode != 2) {
-        psm_joint_current[0] = msg.position[0];  // outer_yaw_joint
-        psm_joint_current[1] = msg.position[1];  // outer_pitch_joint
-        psm_joint_current[2] = msg.position[7];  // outer_insertion_joint
-        psm_joint_current[3] = msg.position[8];  // outer_roll_joint
-        psm_joint_current[4] = msg.position[9];  // outer_wrist_pitch_joint
-        psm_joint_current[5] = msg.position[10];  // outer_wrist_yaw_joint
-        //    psm_joint_current[6] = msg.position[11]; // outer_wrist_open_angle_joint
-    }
-}
-
-void psm_mode_cb(const std_msgs::Int8 &msg)
-{
-    if (msg.data >= 0 && msg.data <=PSM::MODE_TELEOP) {
-        control_mode = msg.data;
-        std::cout << "control_mode = " << control_mode << std::endl;
-    }
-
-    // mode = TELEOP
-    if (msg.data == PSM::MODE_TELEOP) {
-        psm_joint_command.Assign(psm_joint_current);
-        psm_pose_command.Assign(psm_pose_current);
-
-        std::cout << "psm_pose_command = " << psm_pose_command << std::endl;
-    }
-}
+enum { NUM_ROWS=6, NUM_COLS=6};
+vctDynamicMatrix<double> JacobMat(NUM_ROWS, NUM_COLS, 0.0 );
 
 int main(int argc, char** argv)
 {
-    // ros initialization
-    ros::init(argc, argv, "dvrk_psm_kinematics");
-    ros::NodeHandle nh, nh_private("~");
-    ros::Rate rate(200);  // 200 hz rate
-
-    // parameter
-    std::string robot_name;
-    nh_private.getParam("robot_name", robot_name);
-
-    // subscriber
-    ros::Subscriber sub_psm_cmd =
-            nh.subscribe("/dvrk_psm/cartesian_pose_command", 1, psm_cmd_pose_cb);
-    ros::Subscriber sub_mtm_gripper =
-        nh.subscribe("/dvrk_mtm/gripper_position", 1, mtm_gripper_cb);
-
-    ros::Subscriber sub_psm_fb =
-            nh.subscribe("/dvrk_psm/joint_states", 1, psm_joint_feedback_cb);
-
-    ros::Subscriber sub_mode =
-            nh.subscribe("/dvrk_psm/control_mode", 1, psm_mode_cb);
-
-    // publisher
-    ros::Publisher pub_psm_joint_state_cmd =
-            nh.advertise<sensor_msgs::JointState>("/dvrk_psm/joint_states_command", 1);
-    ros::Publisher pub_psm_pose_current =
-            nh.advertise<geometry_msgs::Pose>("/dvrk_psm/cartesian_pose_current", 1);
-    ros::Publisher pub_psm_enable_slider =
-            nh.advertise<sensor_msgs::JointState>("/dvrk_psm/joint_state_publisher/enable_slider", 100);    
-
-    // --- cisst robManipulator ---
-    std::string filename = ros::package::getPath("dvrk_robot");
+    
+     // --- cisst robManipulator ---
+    string filename = ros::package::getPath("dvrk_robot");
     filename.append("/config/dvpsm.rob");
     robManipulator psm_manip;
     robManipulator::Errno result;
     result = psm_manip.LoadRobot(filename);
-    if (result == robManipulator::EFAILURE) {
-        ROS_ERROR("failed to load manipulator config file: %s", filename.c_str());
-    } else {
-        ROS_INFO("loaded psm manipulator");
-    }
+
+
 
     // --- initialize joint current/command -----
     psm_joint_current.SetSize(6);  // 6 + 1 (open angle)
     psm_joint_current.SetAll(0.0);
+
+    psm_joint_currentIK.SetSize(6);  // 6 + 1 (open angle)
+    psm_joint_currentIK.SetAll(0.0);
+
+
     psm_joint_command.ForceAssign(psm_joint_current);
 
-    sensor_msgs::JointState msg_js;
-    msg_js.name.clear();
-    msg_js.name.push_back(robot_name + "_outer_yaw_joint");
-    msg_js.name.push_back(robot_name + "_outer_pitch_joint");
-    msg_js.name.push_back(robot_name + "_outer_insertion_joint");
-    msg_js.name.push_back(robot_name + "_outer_roll_joint");
-    msg_js.name.push_back(robot_name + "_outer_wrist_pitch_joint");
-    msg_js.name.push_back(robot_name + "_outer_wrist_yaw_joint");
-    msg_js.name.push_back(robot_name + "_outer_wrist_open_angle_joint");
-
-    geometry_msgs::Pose msg_pose;
-
-    int count = 0;
-    control_mode = PSM::MODE_RESET;  // start with reset_mode
+   
     vctFrm4x4 frame6to7;
     frame6to7.Assign(0.0, -1.0, 0.0, 0.0,
                      0.0,  0.0, 1.0, 0.0102,
                      -1.0, 0.0, 0.0, 0.0,
                      0.0,  0.0, 0.0, 1.0);
 
-    // used to compensate joint 4
-    double j4_compensate = 0;
 
     // ------------ run() --------------------------
-    while (ros::ok()) {
+   
+         cout << result << endl;
 
+        vctFrm4x4 pose6, pose7, psm_pose0to6, psm_pose0to7, psm_pose0to7_GivenPose;
         // --------- Compute current pose & publish ----------
-        // psm forward kinematics
-        psm_pose_current = psm_manip.ForwardKinematics(psm_joint_current);
-        mtsCISSTToROS(psm_pose_current, msg_pose);
+        // psm forward kinematics'
+        psm_joint_current[2] = 0.0;
+        psm_pose0to6 = psm_manip.ForwardKinematics(psm_joint_current);
+
+
+
+        cout << "Home position" << endl;
+        cout << "FK fram0to6:" << endl;
+        cout << psm_pose0to6 << endl;
 
         // publish current pose
-        pub_psm_pose_current.publish(msg_pose);
 
         // ---------- Compute command psm joint positin -----------
-        vctFrm4x4 pose6;
+
+        psm_pose0to7 = psm_pose0to6 * frame6to7;
+
+        cout << "FK result:" << endl;
+        cout << psm_pose0to7 << endl;
+
+
+        psm_joint_current[0]  = -0.23;
+        psm_joint_current[1]  =  -0.31;
+        psm_joint_current[2]  = 0.1;
+        psm_joint_current[3]  = -0.14;
+        psm_joint_current[4]  = 1.19;
+        psm_joint_current[5]  = 1.11;
+        psm_joint_current[6]  = 0;
+
+        psm_pose0to6 = psm_manip.ForwardKinematics(psm_joint_current);
+
+        cout << "using joints: " << psm_joint_current << endl;
+        cout << "FK fram0to6:" << endl;
+        cout << psm_pose0to6 << endl;
+        psm_pose0to7 = psm_pose0to6 * frame6to7;
+        cout << "FK result:" << endl;
+        cout << psm_pose0to7 << endl;
+
+
+        psm_pose0to7_GivenPose.Assign( -0.2265,    0.4496,    0.8641,   -0.0097,
+                             0.6337,    0.7417,  -0.2198,   0.0166,
+                            -0.7397,    0.4978,   -0.4529,   -0.0888,
+                             0,         0,         0,    1.0000);
+
+
+        psm_pose0to6 = psm_pose0to7 *  frame6to7.InverseSelf();
+
+        cout << "InverseTest" << endl;
+        cout << "FK result:" << endl;
+        cout << psm_pose0to7 << endl;
+        cout << "FK fram0to6:" << endl;
+        cout << psm_pose0to6 << endl;
+
+
+        double tolerance=1e-12;
+        size_t Niteration=1000;
+
+        //psm_manip.InverseKinematics(psm_joint_currentIK, psm_pose0to6, tolerance, Niteration);
+        psm_manip.InverseKinematics(psm_joint_currentIK, psm_pose0to6, tolerance, Niteration);
+
+        cout << "original input joints: " << endl;
+        cout << psm_joint_current << endl;
+
+        cout << "IK result joints " << endl;
+        cout << psm_joint_currentIK << endl;
+
+        cout << "IK error: "<< endl;
+        cout << psm_joint_current - psm_joint_currentIK << endl;
+
+        bool TestJacobian;
+
+
+        //psm_joint_currentTest[0] = psm_joint_current[0];
+        //psm_joint_currentTest[1] = psm_joint_current[1];
+        //psm_joint_currentTest[2] = psm_joint_current[2];
+        //psm_joint_currentTest[3] = psm_joint_current[3];
+        //psm_joint_currentTest[4] = psm_joint_current[4];
+        //psm_joint_currentTest[5] = psm_joint_current[5];
+        
+        //cout << "jacobian joints test" << endl;
+        //cout << psm_joint_currentTest  << endl;
+
+
+        /*
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++){
+                myMatrix.at(i,j) = 0.0;
+            }
+        }*/
+        cout << "myMatrix" << JacobMat << endl;
+        
+        
+        TestJacobian  = psm_manip.JacobianBody(psm_joint_current,JacobMat);
+
+        
+
+        cout << "Jacobian Body Matrix: "<< endl;
+        cout << JacobMat << endl;
+        cout << "Jacobian bool result:" << TestJacobian << endl;
+        
+
 
         // MODE_RESET: send HOME joint position
         // MODE_MANUAL: controled by JSP GUI, not sending anything to jsp
         // MODE_HOLD: disable JSP GUI, not sending anything to jsp
         // MODE_TELEOP: take command pose, send to jsp
 
-        switch(control_mode)
-        {
-        case PSM::MODE_RESET:
-            psm_joint_command.SetAll(0.0);
-            psm_joint_command[2] = 0.10;
-            j4_compensate = 0;
-            psm_pose_command.Assign(psm_pose_current);
-            break;
-        case PSM::MODE_MANUAL:
-            // do nothing for MANUAL
-            // controlled using Slifer GUI
-            j4_compensate = 0;
-            psm_pose_command.Assign(psm_pose_current);
 
-            std::fill(msg_js.position.begin(), msg_js.position.end(), 1);
-            pub_psm_enable_slider.publish(msg_js);
-            break;
-        case PSM::MODE_HOLD:
-            psm_pose_command.Assign(psm_pose_current);
-            std::fill(msg_js.position.begin(), msg_js.position.end(), -1);
-            pub_psm_enable_slider.publish(msg_js);
-            break;
-        case PSM::MODE_TELEOP:
-            // psm_pose_command updated in callback!
-            pose6 = psm_pose_command;
-            psm_manip.InverseKinematics(psm_joint_command, pose6);
-
-            // joint 4 is a special case
-            if (psm_joint_command_prev[3] - psm_joint_command[3] > 5) {
-                j4_compensate = 2 * cmnPI;
-            } else if (psm_joint_command_prev[3] - psm_joint_command[3] < -5) {
-                j4_compensate = - 2 * cmnPI;
-            } else {
-                j4_compensate = 0;
-            }
-            psm_joint_command[3] += j4_compensate;
-            psm_joint_current.Assign(psm_joint_command);
-
-            // disable slider
-            std::fill(msg_js.position.begin(), msg_js.position.end(), -1);
-            pub_psm_enable_slider.publish(msg_js);
-            break;
-        default:
-            ROS_ERROR_STREAM("Should not come here !");
-            break;
-        }
-
-        psm_joint_command_prev.ForceAssign(psm_joint_command);
-
-        // -----  Assign command joint state and Publish --------
-        // 6 + open angle (gripper)
-        msg_js.position.resize(psm_joint_command.size() + 1);
-        std::copy(psm_joint_command.begin(), psm_joint_command.end(), msg_js.position.begin());
-        msg_js.position[6] = mtm_gripper;
-
+        // do nothing for MANUAL
+        // controlled using Slifer GUI
+        /*
         if (control_mode != PSM::MODE_MANUAL && control_mode != PSM::MODE_HOLD) {
             pub_psm_joint_state_cmd.publish(msg_js);
         }
+        */
 
-        ros::spinOnce();
-        rate.sleep();
-        count++;
-    }
+
 
     return 0;
 }
--- dvrk_needle_planner/tutorial/cisstPythonTest.py
+++ dvrk_needle_planner/tutorial/cisstPythonTest.py
@@ -8,8 +8,21 @@ from cisstVectorPython import vctFrm3
 import cisstRobotPython
 from cisstRobotPython import robManipulator
 
+import rospkg
+import numpy as np
+from numpy.linalg import inv
+import os
 
-import numpy
+
+
+from copy import *
+
+pathTest = os.path.abspath(os.path.dirname(__file__))
+filePath = pathTest + '/dvpsm.rob'
+print filePath
+
+
+rospack = rospkg.RosPack()
 
 frame = vctFrm3()
 inverseFrame = vctFrm3()
@@ -22,14 +35,131 @@ translation =  frame.Translation()
 translation[0] = 2.0
 translation[1] = 1.0
 translation[2] = 3.0
-robMap = robManipulator()
 
+np.set_printoptions(precision=4,suppress=True)
+
+psm_manip = robManipulator()
+
+result = psm_manip.LoadRobot(filePath);
+print result
+frame6to7 = np.matrix([[0.0, -1.0, 0.0, 0.0],
+                     [0.0,  0.0, 1.0, 0.0102],
+                     [-1.0, 0.0, 0.0, 0.0],
+                     [0.0,  0.0, 0.0, 1.0]])
+
+
+qvec = np.zeros(7)
+dvrk_FK_0to6_array = psm_manip.ForwardKinematics(qvec)
+dvrk_FK_0to6 = np.matrix(dvrk_FK_0to6_array)
+
+print "Home position"
+print "frame0to6: \n", frame6to7
+#print type(dvrk_FK_0to6)
+dvrk_FK_Test = dvrk_FK_0to6 * frame6to7
+print "frame0to7: \n", dvrk_FK_Test 
+
+#[0.0, 0.0, 0.15, 0.0, 0.0, 0.0]
+
+#qvec_withGripper = np.array([-0.23, -0.31, 0.1, -0.14, 1.19, 1.11, 0.0])
+qvec = np.array([-0.23, -0.31, 0.1, -0.14, 1.19, 1.11])
+qvecIK = np.array([0., 0., 0., 0., 0., 0.])
+
+dvrk_FK_0to6_array = psm_manip.ForwardKinematics(qvec)
+dvrk_FK_0to6 = np.matrix(dvrk_FK_0to6_array)
+
+
+
+print "FK Test"
+print "using joints:", qvec
+print "frame0to6 from FK: \n", dvrk_FK_0to6
+#print type(dvrk_FK_0to6)
+dvrk_FK_Test = dvrk_FK_0to6 * frame6to7
+print "frame0to7 from FK: \n", dvrk_FK_Test 
+#print type(dvrk_FK_Test)
+
+poseGivenTest = np.matrix([
+    [-0.2265,    0.4496,    0.8641,   -0.0097],
+    [0.6337,    0.7417,   -0.2198,    0.0166],
+    [-0.7397,    0.4978,   -0.4529,   -0.0888],
+         [0,         0,         0,    1.0000]])
+
+print "IK Test"
+print "givenPose frame0to7: \n", dvrk_FK_Test
+#print type(dvrk_FK_0to6)
+dvrk_FK_0to6 = dvrk_FK_Test * inv(frame6to7)
+print "frame0to6 from givenPose(frame0to7) using inverse of frame6to7: \n", dvrk_FK_0to6 
+
+
+tolerance=1e-12;
+Niteration=1000;
+
+psm_manip.InverseKinematics(qvecIK, dvrk_FK_0to6);
+
+#qvecIK_withGripper = np.array([qvecIK[0], qvecIK[1], qvecIK[2], qvecIK[3], qvecIK[4], qvecIK[5], 0. ])
+qErr = (qvec - qvecIK)
+
+print "original input joints: ", qvec
+print "IK result joints ", qvecIK
+print "IK error: ",  qErr
+
+Jarray = np.zeros((6, 6))
+Jmat = np.matrix(Jarray)
+#qTest = qvec[0:-1]
+#print qTest
+TestJacobian = psm_manip.JacobianBody(qvec, Jmat)
+print "Jacobian bool result: ", TestJacobian
+print "Jacobian matrix \n", Jmat
+
+
+'''
+
+tolerance=1e-12
+;Niteration=1000;
+
+frame0to6_cisst = vctFrm3()
+
+rotation = frame0to6_cisst.Rotation()
+translation =  frame0to6_cisst.Translation()
+N = 3
+
+
+for i in range(N):
+	for j in range(N):
+		rotation[i, j] = dvrk_FK_0to6.item((i, j))
+
+	translation[i] = dvrk_FK_0to6.item((i, j+1))
+
+
+print "hop"
+print dvrk_FK_0to6
+'''
+
+
+'''
+psm_manip.InverseKinematics(frame0to6_cisst, qvecIK);
+
+print "original input joints: ", qvec
+print "IK result joints ", qvecIK
+print "IK error: ",  (qvec - qvecIK)
+'''
+
+'''
 inverseFrame = frame.InverseSelf()
-q1 = numpy.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0])
-q = numpy.array([2.0,0.0,-0.060,0.0,0.0,0.0,1.0])
-a = robMap.ForwardKinematics(q)
+
+q1 = np.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0])
+q = np.array([2.0,8.0,-0.060,0.0,10.0,0.0,1.0])
+qtest = np.zeros(6)
+q2 = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+
+
+dvrk_FK_0to6 = psm_manip.ForwardKinematics(qtest)
+dvrk_FK_Test = dvrk_FK_0to6 * frame6to7
 #print frame
-print robMap.InverseKinematics(q,frame)
+q2 = psm_manip.InverseKinematics(q,frame)
+print q2
+print type(dvrk_FK_Test)
 #print inverseFrame
-print a
-#print q
+np.set_printoptions(precision=4,suppress=True)
+print dvrk_FK_Test
+#print '%0.2f' % dvrk_FK_Test
+#print q'''
